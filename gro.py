0c1d6caa924b156419eceebb1e2532a7701f0176
nasri <nasri.hamouda.nasri@gmail.com>
2018-09-02 14:07:55 +0100

    Added comments

--- gro.py
+++ gro.py
@@ -1,7 +1,15 @@
 import shelve
+# Make our data persistent
 with shelve.open('drows.db') as s:
-    s['drows'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
+    # Created first dict
+    # And make a dict of dicts
+    s['drows'] = {
+        'foo':{1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'},
+        'bar':{1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
+    } 
+
 with shelve.open('drows.db') as s:
     drow = s['drows']
+    
 for item in drow:
     print(drow[item])
