c227351320002e681ebd5cc89ab80afd6419ed2a
nasri <nasri.hamouda.nasri@gmail.com>
2018-09-02 13:54:17 +0100

    Added use of Python shelve

--- gro.py
+++ gro.py
@@ -1,3 +1,7 @@
-mydict = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
-for item in mydict:
-    print(mydict[item])
+import shelve
+with shelve.open('drows.db') as s:
+    s['drows'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
+with shelve.open('drows.db') as s:
+    drow = s['drows']
+for item in drow:
+    print(drow[item])
