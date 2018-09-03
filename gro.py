ecfaf0a55d06bda7ca9d7a870898b84dd0ec66cf
nasri <nasri.hamouda.nasri@gmail.com>
2018-09-03 12:02:16 +0100

    Added delete row function

--- gro.py
+++ gro.py
@@ -4,13 +4,21 @@ def main():
     # Created first dict
     # And make a dict of dicts
     with shelve.open('drows.db') as s:
+        
         s['foo'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
         s['bar'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
 
     s = shelve.open('drows.db')
 
     for item in s:
-        print(s[item])
+        # print(item + ": " + str(s[item]))
+        print("%s: %s" % (item, s[item])) # this is a tuple
 
+def delrow(s, rowkey):
+    try:
+        del s[rowkey]
+    except:
+        pass
+        
 if __name__ == "__main__":
     main() 
