6752e82d21d165a85c44248e32b1dd42fc425f55
nasri <nasri.hamouda.nasri@gmail.com>
2018-09-03 11:49:30 +0100

    Switched shelve keys to represent rows

--- gro.py
+++ gro.py
@@ -1,15 +1,17 @@
-import shelve
-# Make our data persistent
-with shelve.open('drows.db') as s:
+def main():
+    import shelve
+    # Make our data persistent
     # Created first dict
     # And make a dict of dicts
-    s['drows'] = {
-        'foo':{1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'},
-        'bar':{1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
-    } 
+    with shelve.open('drows.db') as s:
+        s['foo'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
+        s['bar'] = {1:'foo', 2:'bar', 3:'Lumberjack', 4:'I work all day'}
 
-with shelve.open('drows.db') as s:
-    drow = s['drows']
-    
-for item in drow:
-    print(drow[item])
+    s = shelve.open('drows.db')
+
+    for item in s:
+        print(s[item])
+        print('***')
+
+if __name__ == "__main__":
+    main() 
