9445d35b488cad79d243509843dc09fab39d4936
nasri <nasrimohamed147@gmail.com>
2018-09-06 17:30:47 +0100

    edited evalfunc

--- gro.py
+++ gro.py
@@ -98,7 +98,9 @@ def processrow(rownum, arow):
         for coldex, acell in enumerate(arow):
             if globs.row1[coldex] in globs.funcslc:
                 if acell == '?':
-                    evalfunc(coldex, arow)        
+                    arow[coldex] = evalfunc(coldex, arow)        
+                    #print(replaceqmwith)
+    print(arow)                
 
 def row1funcs(arow):
     """Scans row-1 for names of global functions and builds dict of requirements
@@ -144,7 +146,8 @@ def evalfunc(coldex, arow):
     else:
         #No arguments required, so just immediately close the parenthesis.    
         evalme = evalme + ')'
-    print('%s: %s' % (evalme, eval(evalme)))
+    # return('%s: %s' % (evalme, eval(evalme)))
+    return(eval(evalme))
 
 
 def getargval(anarg, defargval, arow):
@@ -179,10 +182,10 @@ def adq(aval):
 
 
 def Func1():
-    return "No arguments here" 
+    return "Out from func1" 
 
 def Func2(param1, param2='', status='Okay'):
-    return "My params are: %s, %s" % (param1, param2)        
+    return "%s %s" % (param1, param2)        
 
 if __name__ == "__main__":
     main()
